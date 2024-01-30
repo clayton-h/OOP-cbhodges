1ce85fb95e4bd5d158dca304cbba0318e7aa2103
ClayT <claytonhodges326@gmail.com>
2024-01-30 13:02:54 -0700

    kattis_cli

--- sorttwonumbers/test_sortnums.py
+++ sorttwonumbers/test_sortnums.py
@@ -5,7 +5,7 @@
 # By: Clayton H.
 #
 
-import sortnums
+import sorttwonumbers.sorttwonumbers as sorttwonumbers
 import unittest
 
 
@@ -13,27 +13,27 @@ class TestFunctions(unittest.TestCase):
     def test1(self) -> None:
         print("\nTest 1...")
         num_list = [10, 5]
-        result = sortnums.sort(num_list)
+        result = sorttwonumbers.sort(num_list)
         expected = [5, 10]
         self.assertEqual(result, expected)
 
     def test2(self) -> None:
         print("\n\nTest 2...")
         num_list = [-5, -10]
-        result = sortnums.sort(num_list)
+        result = sorttwonumbers.sort(num_list)
         expected = [-10, -5]
         self.assertEqual(result, expected)
 
     def test3(self) -> None:
         print("\n\nTest 3...")
         num_list = [10, -5]
-        result = sortnums.sort(num_list)
+        result = sorttwonumbers.sort(num_list)
         expected = [-5, 10]
         self.assertEqual(result, expected)
 
     def test4(self) -> None:
         print("\n\nI/O test begin...")
-        sortnums.test()
+        sorttwonumbers.test()
 
 
 if __name__ == "__main__":
