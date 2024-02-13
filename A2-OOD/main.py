674df35cd935c6a7b2f1648cb6a7ddd96a46156d
ClayT <claytonhodges326@gmail.com>
2024-02-13 16:29:51 -0700

    branch_created

---
+++ A2-OOD/main.py
@@ -0,0 +1,22 @@
+# nano_create
+class vertex:
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+
+    def print_area(self) -> None:
+        print(f"{self.x} + {self.y}")
+
+class polygon:
+    def __init__() -> None:
+        
+
+
+def main() -> None:
+    print("Enter # complex polygons")
+    numpoly = input()
+
+
+
+if __name__ == "__main__":
+    main()
