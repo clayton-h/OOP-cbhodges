602e3d0acec652e27e2141986da5de332a60213d
ClayT <claytonhodges326@gmail.com>
2024-02-29 14:11:00 -0700

    testing

--- A3-unittesting/morsecodepalindromes/test_morsecodepalindrome.py
+++ A3-unittesting/morsecodepalindromes/test_morsecodepalindrome.py
@@ -21,12 +21,17 @@ class TestMorse(unittest.TestCase):
     def test_1(self) -> None:
         """test 1
         """
-        assert (is_morse_pal('$%@#hello') == 0)
+        assert (is_morse_pal('$hello') == 0)
 
     def test_2(self) -> None:
         """test 2
         """
-        assert (is_morse_pal('$%@#hello') == 0)
+        assert (is_morse_pal('Madam I\'m Adam') == 0)
+
+    def test_3(self) -> None:
+        """test 3
+        """
+        assert (is_morse_pal('159') == 1)
 
 
 if __name__ == "__main__":
