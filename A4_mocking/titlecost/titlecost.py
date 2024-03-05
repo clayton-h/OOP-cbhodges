52dda861d514a9244d432e9ccca3e9af8665be15
ClayT <claytonhodges326@gmail.com>
2024-03-05 13:20:18 -0700

    further_setup

--- A4_mocking/titlecost/titlecost.py
+++ A4_mocking/titlecost/titlecost.py
@@ -6,3 +6,14 @@
 #
 # By: Clayton H.
 #
+
+def main() -> None:
+    """Main driver takes user input
+    and passes it to the appropriate
+    functions for processing.
+    """
+    print("main")
+
+
+if __name__ == "__main__":
+    main()
