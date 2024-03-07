54dc6507f57bd77bd4456ea34ae9bec367848158
ClayT <claytonhodges326@gmail.com>
2024-03-07 13:42:14 -0700

    testing

--- A4_mocking/test/test_titlecost.py
+++ A4_mocking/test/test_titlecost.py
@@ -6,6 +6,7 @@
 #
 
 import unittest
+from unittest.mock import patch
 from hypothesis import given, settings, strategies as st
 from A4_mocking.titlecost.modules.calculate import CostCalculator
 
@@ -15,9 +16,16 @@ class TestCostCalculator(unittest.TestCase):
 
     def test_cost_calculator_known_cases(self) -> None:
         """Test the cost calculator with known cases."""
-        self.assertEqual(CostCalculator('movie 10').get_cost(), 5)
-        self.assertEqual(CostCalculator('longtitle 7').get_cost(), 7)
-        self.assertEqual(CostCalculator('short 100').get_cost(), 5)
+        with patch('A4_mocking.titlecost.modules.calculate.CostCalculator') \
+                as MockCalculator:
+            MockCalculator.return_value.get_cost.return_value = 1
+            self.assertEqual(CostCalculator('movie 1').get_cost(), 1)
+
+            MockCalculator.return_value.get_cost.return_value = 7
+            self.assertEqual(CostCalculator('longtitle 7').get_cost(), 7)
+
+            MockCalculator.return_value.get_cost.return_value = 4
+            self.assertEqual(CostCalculator('same 4').get_cost(), 4)
 
     @given(st.text(alphabet=st.characters(min_codepoint=97, max_codepoint=122),
                    min_size=1),
@@ -27,10 +35,23 @@ class TestCostCalculator(unittest.TestCase):
             -> None:
         """Test the cost calculator with random title and cap values."""
         s = f"{title} {cap}"
-        calculator = CostCalculator(s)
-        result = calculator.get_cost()
-        self.assertTrue(0 <= result <= cap, f"Result for '{s}' should be \
-                        between 0 and {cap}")
+        with patch('A4_mocking.titlecost.modules.calculate.CostCalculator') \
+                as MockCalculator:
+            MockCalculator.return_value.get_cost.return_value = len(title)
+            calculator = CostCalculator(s)
+            result = calculator.get_cost()
+            self.assertTrue(result <= len(title),
+                            f"Result for '{s}' should be less than or equal to\
+                                  {result}")
+
+    def test_setter_getter(self) -> None:
+        """Test the calculator getter and setter methods."""
+        with patch('A4_mocking.titlecost.modules.calculate.CostCalculator') \
+                as MockCalculator:
+            MockCalculator.return_value.s = 'spoderman'
+            calculator = CostCalculator('')
+            calculator.s = 'spoderman'
+            self.assertEqual(calculator.s, 'spoderman')
 
 
 if __name__ == "__main__":
