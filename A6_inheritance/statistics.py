1ab2502b354f0ce59bca42b6a0e1dbe8eda79121
ClayT <claytonhodges326@gmail.com>
2024-04-02 13:02:04 -0600

    initial_commit

---
+++ A6_inheritance/statistics.py
@@ -0,0 +1,16 @@
+#
+# Python script that calculates
+# the min, max, and range of
+# user-provided values
+#
+# By: Clayton H.
+#
+
+def main() -> None:
+    data = []
+    data.append(input())
+    print(data)
+
+
+if __name__ == "__main__":
+    main()
